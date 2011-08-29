2464d87c7030c6844f85f157aab043945fb3a03f
Patrick Strawderman <patrick@kilink.net>
2011-08-29 17:51:41 -0400

    add test, get packaging working

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name="ghdiff",
-    version="0.1",
+    version="0",
     author="Patrick Strawderman",
     author_email="patrick@kilink.net",
     url="http://github/kilink/ghdiff",
     license="MIT",
-    package_dir={"":"src"},
+    package_data={"": ["*.py", "*.txt", "*.css"]},
+    include_package_data=True,
+    package_dir={"": "src"},
     py_modules=["ghdiff"],
-    package_data={"ghdiff": ["*.txt", "*.css"]},
     )
