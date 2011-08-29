f65ce91caff69e2844eb96dceaa68e9b8dcd2684
Patrick Strawderman <patrick@kilink.net>
2011-08-29 17:52:04 -0400

    revert last commit This reverts commit 2464d87c7030c6844f85f157aab043945fb3a03f.

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup
 
 setup(
     name="ghdiff",
-    version="0",
+    version="0.1",
     author="Patrick Strawderman",
     author_email="patrick@kilink.net",
     url="http://github/kilink/ghdiff",
     license="MIT",
-    package_data={"": ["*.py", "*.txt", "*.css"]},
-    include_package_data=True,
-    package_dir={"": "src"},
+    package_dir={"":"src"},
     py_modules=["ghdiff"],
+    package_data={"ghdiff": ["*.txt", "*.css"]},
     )
