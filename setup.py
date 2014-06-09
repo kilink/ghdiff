9ea024b892fd507039c8f3be0ff7c757da6f3cbd
Martín Gaitán <gaitan@gmail.com>
2014-06-09 02:32:37 -0300

    ipython classifier

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.3',
                    'Programming Language :: Python :: 3.4',
+                   'Framework :: IPython',
                    'Topic :: Software Development',
                    'Topic :: Utilities',
                    ],
